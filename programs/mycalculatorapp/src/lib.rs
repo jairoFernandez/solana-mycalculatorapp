84e9c34aa00be451ad4ffdc42114ea12a70cde02
Jairo Fernandez <jairofg12@gmail.com>
2022-03-10 17:47:20 -0500

    adding test

--- programs/mycalculatorapp/src/lib.rs
+++ programs/mycalculatorapp/src/lib.rs
@@ -6,35 +6,45 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod mycalculatorapp {
     use super::*;
 
-    pub fn create(ctx: Context<Create>, init_message: String) -> ProgramResult {
+    pub fn create(ctx: Context<Create>, init_message: String) -> Result<()> {
         let calculator = &mut ctx.accounts.calculator;
         calculator.greeting = init_message;
         Ok(())
     }
 
-    pub fn add(ctx: Context<Addition>, num1: i64, num2: i64) -> ProgramResult {
+    pub fn add(ctx: Context<Addition>, num1: i64, num2: i64) -> Result<()> {
         let calculator = &mut ctx.accounts.calculator;
         calculator.result = num1 + num2;
         Ok(())
     }
 
-    pub fn multiply(ctx: Context<Multiplication>, num1: i64, num2: i64) -> ProgramResult {
+    pub fn multiply(ctx: Context<Multiplication>, num1: i64, num2: i64) -> Result<()> {
         let calculator = &mut ctx.accounts.calculator;
         calculator.result = num1 * num2;
         Ok(())
     }
 
-    pub fn subtract(ctx: Context<Subtraction>, num1: i64, num2: i64) -> ProgramResult {
+    pub fn subtract(ctx: Context<Subtraction>, num1: i64, num2: i64) -> Result<()> {
         let calculator = &mut ctx.accounts.calculator;
         calculator.result = num1 - num2;
         Ok(())
     }
 
-    pub fn divide(ctx: Context<Division>, num1: i64, num2: i64) -> ProgramResult {
+    pub fn divide(ctx: Context<Division>, num1: i64, num2: i64) -> Result<()> {
         let calculator = &mut ctx.accounts.calculator;
         calculator.result = num1 / num2;
+        calculator.remainder = num1 % num2;
         Ok(())
     }
+}
+
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 8 + 64 + 64 + 64 + 64)]
+    pub calculator: Account<'info, Calculator>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
